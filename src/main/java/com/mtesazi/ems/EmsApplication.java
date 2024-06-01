aa7af28d8de74d9d7af554138eb21c063358d0cb
bam <Mtesazi@gmail.com>
2024-06-01 15:23:38 +0200

    Create Employee entity

--- src/main/java/com/mtesazi/ems/EmsApplication.java
+++ src/main/java/com/mtesazi/ems/EmsApplication.java
@@ -1,13 +1,15 @@
 package com.mtesazi.ems;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EmsApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmsApplication.class, args);
 	}
-
 }
