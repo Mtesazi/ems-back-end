bf3aa7cb0163c1c9fa4aa83ce23d614314d79b5c
bam <Mtesazi@gmail.com>
2024-06-02 11:28:49 +0200

    Create get employee by id REST API

--- src/main/java/com/mtesazi/ems/service/EmployeeService.java
+++ src/main/java/com/mtesazi/ems/service/EmployeeService.java
@@ -4,4 +4,5 @@ import com.mtesazi.ems.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee (EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById (Long employeeId);
 }
