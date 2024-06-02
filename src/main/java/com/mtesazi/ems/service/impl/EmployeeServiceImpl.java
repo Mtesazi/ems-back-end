dd13a6fed5b2100f777db82bb582ead8f57e93f5
bam <Mtesazi@gmail.com>
2024-06-02 12:34:32 +0200

    create update employee REST API

--- src/main/java/com/mtesazi/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mtesazi/ems/service/impl/EmployeeServiceImpl.java
@@ -34,4 +34,17 @@ public class EmployeeServiceImpl  implements EmployeeService {
         List<Employee> employees = employeeRepository.findAll();
         return  employees.stream().map(EmployeeMapper::mapToEmployeeDto).collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() ->
+                new ResourceNotFoundException("Employee does not exist with given id :" + employeeId));
+
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setContactNumber(updatedEmployee.getContactNumber());
+        employee.setEmailAddress(updatedEmployee.getEmailAddress());
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
 }
